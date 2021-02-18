7156920d6604e213ca47c6c08505a322fe095e7c
bleeqer <bleeqtheproducer@gmail.com>
2021-02-19 00:44:33 +0900

    done

--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,8 @@ from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .models import Todo
 from .forms import TodoForm
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -30,17 +32,52 @@ def signupuser(request):
             # tell the user the passwords didn't match.
 
 
+@login_required
 def currenttodos(request):
     todos = Todo.objects.filter(todocreator=request.user, datecompleted__isnull=True)
     
     return render(request, 'todo/currenttodos.html', {'todos': todos})
 
 
+@login_required
 def viewtodo(request, todo_pk):
-    todo = get_object_or_404(Todo, pk=todo_pk)
+    todo = get_object_or_404(Todo, pk=todo_pk, todocreator=request.user)
 
-    return render(request, 'todo/viewtodo.html', {'todo': todo})
- 
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+
+        return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form})
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+
+        except ValueError:
+            return render(request, 'todo/createtodo.html', {'todo': todo, 'form': TodoForm(), 'error': 'invalid info'})
+
+@login_required
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, todocreator=request.user)
+    if request.method == 'POST':
+        todo.datecompleted = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
+
+
+@login_required
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, todocreator=request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodos')
+
+
+@login_required
+def completedtodos(request):
+        todos = Todo.objects.filter(todocreator=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+        return render(request, 'todo/completedtodos.html', {'todos': todos})
+
 
 def logoutuser(request):
     logout(request)
@@ -65,6 +102,8 @@ def loginuser(request):
             return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error': 'username and password didn\'t match'})
             
 
+
+@login_required
 def createtodo(request):
     
     if request.method == 'GET':
